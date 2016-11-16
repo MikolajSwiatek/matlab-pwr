b1d6914149cc0aa501c6ecefdb31edf0c506ba4a
Mikołaj Świątek <mikolajswiatek@outlook.com>
2016-11-16 09:01:54 +0100

    refactoring

--- exercise-03/display_hsv.m
+++ exercise-03/display_hsv.m
@@ -1,6 +1,5 @@
 function[] = display_hsv(h, s, v)
     size = 256;
-    rgb = zeros(size, size, 3);
     hsv = zeros(size, size, 3);
 
     hsv(:,:,1) = h;
@@ -8,15 +7,6 @@ function[] = display_hsv(h, s, v)
     hsv(:,:,3) = v;
     
     rgb = hsv2rgb(hsv);
-    
-    disp('HSV (100, 100):');
-    disp(hsv(100, 100, 1));
-    disp(hsv(100, 100, 2));
-    disp(hsv(100, 100, 3));
-    disp('RGB (100, 100):');
-    disp(rgb(100, 100, 1));
-    disp(rgb(100, 100, 2));
-    disp(rgb(100, 100, 3));
     
     figure('Name', 'hsv2rgb');
     subplot(2, 1, 1);
@@ -26,3 +16,8 @@ function[] = display_hsv(h, s, v)
 	subplot(2, 1, 2);
     imshow(rgb);
     title('RGB image');
+    
+    x = 100;
+    y = 100;
+    
+    impixel(rgb, x, y)
