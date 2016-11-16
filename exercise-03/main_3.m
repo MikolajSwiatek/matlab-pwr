a936c488014e841e012ef0cdc4d053ceacf86f3f
Mikołaj Świątek <mikolajswiatek@outlook.com>
2016-11-16 09:35:21 +0100

    Update main_3.m

--- exercise-03/main_3.m
+++ exercise-03/main_3.m
@@ -4,11 +4,45 @@ img = imread(fileName);
 
 YCBCR = rgb2ycbcr(img);
 
-figure('Name', '');
+Y = YCBCR(:, :, 1);
+CB = YCBCR(:, :, 2);
+CR = YCBCR(:, :, 3);
+
+figure
 subplot(2, 2, 1);
 imshow(img);
 title('Input Image');
 
 subplot(2, 2, 2);
-imshow(YCBCR);
-title('YCBCR Image');
+imshow(Y);
+title('Luminance Y');
+
+subplot(2, 2, 3);
+imshow(CB);
+title('Chroma CB');
+
+subplot(2, 2, 4);
+imshow(CR);
+title('Chroma CR');
+
+CB = down_sampling(CB);
+CR = down_sampling(CR);
+CB = up_sampling(CB);
+CR = up_sampling(CR);
+
+figure
+subplot(2, 2, 1);
+imshow(img);
+title('Input Image');
+
+subplot(2, 2, 2);
+imshow(Y);
+title('Luminance Y');
+
+subplot(2, 2, 3);
+imshow(CB);
+title('Chroma CB Reconstructed');
+
+subplot(2, 2, 4);
+imshow(CR);
+title('Chroma CR Reconstructed');
