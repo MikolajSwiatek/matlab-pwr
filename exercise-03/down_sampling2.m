d26847b057f05eba190aa9e77d725a3584303ebc
Mikołaj Świątek <mikolajswiatek@outlook.com>
2016-11-17 13:45:58 +0100

    Update down_sampling2.m

--- exercise-03/down_sampling2.m
+++ exercise-03/down_sampling2.m
@@ -4,7 +4,6 @@ function [Y] = down_sampling2(X, num)
     k = 1;
     l = 1;
     
-    %{
     for i = 1:num:n
         for j = 1:num:m
             Y(k, l) = X(i, j);
@@ -13,17 +12,3 @@ function [Y] = down_sampling2(X, num)
         k = k + 1;
         l = 1;
      end
-    %}
-    
-    for i = 1:n
-        if mod(i, num) == 0
-            for j = 1:m
-                if mod(j, num) == 0        
-                    Y(k, l) = X(i, j);
-                    l = l +1 ;
-                end
-            end
-            k = k + 1;
-            l = 1;
-        end
-    end
