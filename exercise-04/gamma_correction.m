9c8c2848ea258a5bbe400c15e94171d37886d386
Mikołaj Świątek <mikolajswiatek@outlook.com>
2016-11-24 10:16:13 +0100

    Update gamma_correction.m

--- exercise-04/gamma_correction.m
+++ exercise-04/gamma_correction.m
@@ -1,7 +1,16 @@
-function[Y] = gamma_correction(X, gamma_r, gamma_g, gamma_b)
+function[Y] = gamma_correction(X, gamma_r, gamma_g, gamma_b, C)
     imgDouble = im2double(X);
+    
     R = imgDouble(:, :, 1);
     G = imgDouble(:, :, 2);
     B = imgDouble(:, :, 3);
-    
-    
+	
+	R = arrayfun(@(x) C*x^gamma_r, R);
+	G = arrayfun(@(x) C*x^gamma_g, G);
+	B = arrayfun(@(x) C*x^gamma_b, B);
+	
+	Y(:, :, 1) = R;
+	Y(:, :, 2) = G;
+	Y(:, :, 3) = B;
+	
+	Y = im2uint8(Y);
