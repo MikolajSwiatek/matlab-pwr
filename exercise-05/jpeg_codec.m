92a71b977620ea5db291821eacc7f5ece1e38472
Mikołaj Świątek <mikolajswiatek@outlook.com>
2016-11-26 11:04:08 +0100

    Add files via upload

---
+++ exercise-05/jpeg_codec.m
@@ -0,0 +1,21 @@
+fileName = 'Lena_gray_8.tif';
+img = imread(fileName);
+
+imgDouble = double(img);
+imgDouble = arrayfun(@(x) x - 128, imgDouble);
+
+B = blkproc(imgDouble, [8, 8], 'dct2');
+
+Q = blkproc(B, [8, 8], 'quantization(B)');
+D = blkproc(Q, [8, 8], 'dequantization(B)');
+E = blkproc(D, [8, 8], 'idct2');
+
+E = arrayfun(@(x) (x + 128)/255, E);
+final = im2uint8(E);
+
+figure;
+subplot(1, 2, 1);
+imshow(img);
+
+subplot(1, 2, 2);
+imshow(final);
